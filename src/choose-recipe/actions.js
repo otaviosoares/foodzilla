5fc93090efdd4a39022b6f13f016a2cde91d9d80
Otavio Augusto <otaviokk@gmail.com>
2017-11-29 10:06:48 -0200

    fix url

--- src/choose-recipe/actions.js
+++ src/choose-recipe/actions.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001/api';
+const BASE_URL = 'http://35.202.210.121/api';
 
 export function selectRecipe (recipeId) {
-  const url = `${BASE_URL}/recipes?recipeId=${recipeId}`
+  const url = `${BASE_URL}/recipes/${recipeId}`
   return dispatch => (
     axios.get(url)
     .then(response => (
